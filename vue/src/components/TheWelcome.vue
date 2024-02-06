7a0e5875afebb4924a043b6ef36c6a29a2d4758a
jasminew13 <jasminewu101@gmail.com>
2024-02-06 10:12:51 -0500

    a

---
+++ vue/src/components/TheWelcome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{ Destination.name }}</h2>
+    <img :src="Destination.img" alt=""/>
+    <h3>{{ clicked }}</h3>
+    <button @click="increment">click</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps ({
+  Destination: Object,
+})
+//clicker logic
+const clicked = ref(0);
+function increment(){
+  clicked.value++;
+}
+</script>
+
+<style scoped>
+img{
+  width:250px;
+  height: 300px;
+  object-fit: cover;
+}
+
+</style>
