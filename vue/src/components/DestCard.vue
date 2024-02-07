21fd11ffc37a73c020b47852b86bc7c4387fcd39
jasminew13 <jasminewu101@gmail.com>
2024-02-07 10:14:30 -0500

    a

--- vue/src/components/DestCard.vue
+++ vue/src/components/DestCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
+    <div class="cards">
       <h2>{{ Destination.name }}</h2>
-      <img :src="Destination.img" alt=""/>
+      <img :src="Destination.img" :alt="Destination.name"/>
+      <h3>${{ Destination.price }}</h3>
       <h3>{{ clicked }}</h3>
       <button @click="increment">click</button>
     </div>
@@ -25,5 +26,11 @@ img{
     height: 300px;
     object-fit: cover;
 }
+.cards{
+    background-color: rgb(251, 245, 236);
+    align-items: center;
+    justify-content: center;
+}
+
 
 </style>
